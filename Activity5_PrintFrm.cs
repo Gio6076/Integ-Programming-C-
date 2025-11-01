0631a82d46e3044fb599f034d710140116a3e2da
Gio6076 <gebarola@gmail.com>
2025-11-01 15:09:28 +0800

    Activity5_PrintFrm.cs

---
+++ Activity5_PrintFrm.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ebs_Lab_3
+{
+    public partial class Activity5_PrintFrm : Form
+    {
+        public Activity5_PrintFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void payslip_viewListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
